f5080e0aab2baab04b0b6978bcb5225a7eeda8fb
Hazem Mahmoud <hazemhathout1@gmail.com>
2023-09-19 12:05:07 +0300

    External Interrupts

---
+++ MCAL_Layer/Interrupt/mcal_internal_interrupt.h
@@ -0,0 +1,23 @@
+/*
+ * mcal_internal_interrupt.h
+ *
+ *  Created on: Sep 18, 2023
+ *      Author: hazem
+ */
+
+#ifndef MCAL_LAYER_INTERRUPT_MCAL_INTERNAL_INTERRUPT_H_
+#define MCAL_LAYER_INTERRUPT_MCAL_INTERNAL_INTERRUPT_H_
+
+/*---------------------------INCLUDES------------------------*/
+#include "mcal_interrupt_config.h"
+/*------------------------MACRO Declarations----------------*/
+
+/*------------------------MACRO functions Declarations------------------*/
+
+
+/*-------------------------Data Types Declarations---------------------------*/
+
+
+/*-----------------------------Software Interfaces Declarations-----------------------*/
+
+#endif /* MCAL_LAYER_INTERRUPT_MCAL_INTERNAL_INTERRUPT_H_ */
